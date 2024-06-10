62293ea7f30f8e5c0080600fd30dd0bb7532bb2a
Wim Stalmans <wim.stalmans@gmail.com>
2024-06-10 20:42:43 +0200

    basic tailwind setup

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'british-racing-green': '#004225', // Adding British racing green color
+      },
+      backgroundImage: {
+        'gradient-to-r': 'linear-gradient(to right, var(--tw-gradient-stops))',
+        'gradient-to-british-racing-green': 'linear-gradient(to right, #004225, #006400)', // Custom gradient
+      },
+    },
+  },
+  plugins: [],
+}
