ac907e56815a83f5a994ed320288a256877556ec
Wim Stalmans <wim.stalmans@gmail.com>
2024-06-14 05:12:40 +0200

    new routing method

---
+++ src/components/ProfilesPage.tsx
@@ -0,0 +1,9 @@
+import '../styles/main.css';
+
+export default function ProfilesPage() {
+    return (
+      <div className="w-screen h-screen bg-slate-600 text-white">
+        <h1>Profiles</h1>
+      </div>
+    );
+  }
--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
-import App from './App.tsx';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+import ProfilesPage from "./components/ProfilesPage";
+import HomePage from "./components/HomePage";
+import NotFoundPage from "./components/NotFoundPage";
+
+
 import './styles/main.css';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>,
-)
+const router = createBrowserRouter([ 
+    { path: "/", element: <HomePage />, errorElement: <NotFoundPage />},
+    { path: "/profiles", element: <ProfilesPage />},
+]);
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+    <React.StrictMode>
+        <RouterProvider router={router} />
+    </React.StrictMode>,
+);    
